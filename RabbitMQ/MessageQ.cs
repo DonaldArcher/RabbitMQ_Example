39a60a3a28172092cbe881d3366555fc0e46f5e9
Donald Archer <donald.archer@gmail.com>
2022-10-29 20:59:56 +0200

    Clean Up, removed as no singleton in console app

--- RabbitMQ/MessageQ.cs
+++ RabbitMQ/MessageQ.cs
@@ -20,21 +20,5 @@ namespace RabbitMQ
             channel.QueueDeclare(queue: queueName, durable: false, autoDelete: false, exclusive: false, arguments: null);
             channel.BasicPublish("", queueName, null, Encoding.UTF8.GetBytes(message));
         }
-
-        //not working, disposed objects issue.
-        public static void ReceiveMessage(string host, string queueName, string username, string password, EventHandler<BasicDeliverEventArgs> callback)
-        {
-            ConnectionFactory factory = new ConnectionFactory { HostName = host, Port = 5672 };
-            factory.UserName = username;
-            factory.Password = password;
-            factory.VirtualHost = "my_vhost";
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: queueName, durable: false, autoDelete: false, exclusive: false, arguments: null);
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += callback;
-            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
-        }
-       
     }
 }
